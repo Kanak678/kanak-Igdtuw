a21dcb9154dbdb72a3a71232cd868edb37776360
Kanak678 <141045179+Kanak678@users.noreply.github.com>
2024-06-16 13:08:46 +0530

    prog12.py

---
+++ prog12.py
@@ -0,0 +1,10 @@
+def sum_of_digits(number):
+    number_str = str(number)
+    digit_sum = 0
+    for digit in number_str:
+        digit_sum += int(digit)
+    return digit_sum
+number = int(input("Enter a number: "))
+result = sum_of_digits(number)
+print("Sum of digits:", result)
+
