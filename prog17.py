b8a28e55fab38a0a32e0389ad6b81ddb70babdf7
Kanak678 <141045179+Kanak678@users.noreply.github.com>
2024-06-16 13:11:04 +0530

    prog17.py

---
+++ prog17.py
@@ -0,0 +1,5 @@
+def convert_to_title_case(input_string):
+    return input_string.title()
+input_string = input("Enter a string: ")
+title_case_string = convert_to_title_case(input_string)
+print("Title case string:", title_case_string)
