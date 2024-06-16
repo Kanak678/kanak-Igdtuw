67302caf5e4a128a0377d00f62f1d8d586c3d6d4
Kanak678 <141045179+Kanak678@users.noreply.github.com>
2024-06-16 13:07:29 +0530

    prog10.py

---
+++ prog10.py
@@ -0,0 +1,5 @@
+def convert_to_uppercase(input_string):
+    return input_string.upper()
+input_string = input("Enter a string: ")
+uppercase_string = convert_to_uppercase(input_string)
+print("Uppercase string:", uppercase_string)
